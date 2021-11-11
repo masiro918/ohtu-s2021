216737a1e3c0cfb381afe51655ae8f16ec2541b2
Matias Siro <matias.siro@outlook.com>
2021-11-11 14:25:39 +0200

    tehtävän 2 vaatimukset saavutettu

--- vk2/poetry-web/project-reader/src/project_reader.py
+++ vk2/poetry-web/project-reader/src/project_reader.py
@@ -10,8 +10,6 @@ class ProjectReader:
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
-        #print(content)
-        #print("===========")
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
         data = str(toml.loads(content))
         
@@ -25,12 +23,6 @@ class ProjectReader:
         blocks2 = blocks[1].split("dev-dependencies':")
         dev_dependencies = blocks2[1].split("}}, '")
         
-        print("dependencies\n")
-        #print(dependencies[0].replace("{", ""))
-        print("dev_dependencies\n")
-        #print(dev_dependencies[0].replace("{", ""))
-        print("===========")
-        
         str1 = dependencies[0].replace("{", "")
         str1 = str1.replace("}", "")
         str1 = str1.replace("'", "")
@@ -39,12 +31,6 @@ class ProjectReader:
         str2 = str2.replace("}", "")
         str2 = str2.replace("'", "")
         
-        print(str1)
-        print(str2)
-        
-        #deps = []
-        #dev_deps = []
-        
         deps = str1.split(",")
         dev_deps = str2.split(",")
         
@@ -52,37 +38,25 @@ class ProjectReader:
         return_dev_deps = []
         
         i = 0
-        #print(len(deps))
         
         while (i < (len(deps))):
-            #print(i)
             s = deps[i]
             s = s.split(":")
             
-            # TODO: tee oliot!
-            
-            return_deps.append(s[0])
+            return_deps.append(s[0].replace(" ", ""))
             i = i + 1
-            #input("")
             
         i = 0
         
         while (i < (len(dev_deps))):
-            #print(i)
             s = dev_deps[i]
             s = s.split(":")
             
             # TODO: tee oliot!
             
-            return_dev_deps.append(s[0])
+            return_dev_deps.append(s[0].replace(" ", ""))
             i = i + 1
-            #input("")
             
-        for s in return_deps:
-            print(s)
-            
-        for s in return_dev_deps:
-            print(s)
         
-        #print("out")
+            
         return Project("Test name", "Test description", return_deps, return_dev_deps)
