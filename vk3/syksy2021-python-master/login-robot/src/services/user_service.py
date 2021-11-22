42ca6e85440e1cbd5d314ebac574879c9a594176
Matias Siro <matias.siro@outlook.com>
2021-11-22 15:26:24 +0200

    haettu tehtävän 7 pohja

--- vk3/syksy2021-python-master/login-robot/src/services/user_service.py
+++ vk3/syksy2021-python-master/login-robot/src/services/user_service.py
@@ -1,6 +1,6 @@
 from entities.user import User
 import re
-
+import sys, pdb
 
 class UserInputError(Exception):
     pass
@@ -15,6 +15,8 @@ class UserService:
         self._user_repository = user_repository
 
     def check_credentials(self, username, password):
+        pdb.Pdb(stdout=sys.__stdout__).set_trace()
+        
         if not username or not password:
             raise UserInputError("Username and password are required")
 
