5be3e698e7144010a89ac1f4351d4ab3078119cc
Matias Siro <matias.siro@outlook.com>
2021-12-04 13:40:20 +0200

    tehty pistelaskentaohjelma loppuun

--- vk5/tennis/src/index.py
+++ vk5/tennis/src/index.py
@@ -7,34 +7,13 @@ def main():
     player2 = Player("player2")
     
     tennis_court = TennisCourt(player1, player2)
-
+    
     tennis_court.set_point(player1)
     tennis_court.set_point(player1)
     tennis_court.set_point(player2)
     tennis_court.set_point(player1)
     tennis_court.set_point(player1)
-
-
-    '''
-    game = TennisGame("player1", "player2")
-
-    print(game.get_score())
-
-    game.won_point("player1")
-    print(game.get_score())
-
-    game.won_point("player1")
-    print(game.get_score())
-
-    game.won_point("player2")
-    print(game.get_score())
-
-    game.won_point("player1")
-    print(game.get_score())
-
-    game.won_point("player1")
-    print(game.get_score())
-    '''
+    
 
 if __name__ == "__main__":
     main()
